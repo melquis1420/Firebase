fcd276086b7fa4b4b10f08252910704a470a2768
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2022-01-11 11:47:29 -0300

    Using listen function in Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,13 @@ Code => Preferences => Settings => Search setting,
 type in "flutter run additional args", 
 Add item => Add "--no-sound-null-safety"
 */
-void main() {
+void main() async {
+  /*
   //starting firebase
   WidgetsFlutterBinding.ensureInitialized();
 
   //retrieve the database instance
   Firestore db = Firestore.instance;
-
-  /*
 
   //set data manually
   db
@@ -27,9 +26,42 @@ void main() {
       .setData({"nome": "Lara", "idade": "25"});
       */
 
+/*
 //set data automatically
   db.collection("noticias").add(
       {"titulo": "Notícia de teste!!! ", "descricao": "texto de exemplo...."});
+*/
+
+  //db.collection("usuarios").document("003").delete(); //delete
+/*
+ //recover a data.
+  DocumentSnapshot snapshot =
+      await db.collection("usuarios").document("001").get();
+
+  print("dados: " + snapshot.data.toString());
+
+  */
+
+/*
+  QuerySnapshot querySnapshot = await db.collection("usuarios").getDocuments();
+
+//take all data
+  for (DocumentSnapshot item in querySnapshot.documents) {
+    var dados = item.data;
+    print("dados usuarios: " + dados.toString());
+  }
+  */
+
+  WidgetsFlutterBinding.ensureInitialized();
+  Firestore db = Firestore.instance;
+
+//utiliza o "listen" para notificar caso tenha alteração e atualizar a lista de visualização.
+  db.collection("usuarios").snapshots().listen((snapshot) {
+    for (DocumentSnapshot item in snapshot.documents) {
+      var dados = item.data;
+      print("dados usuarios: " + dados.toString());
+    }
+  });
 
   runApp(MyApp());
 }
